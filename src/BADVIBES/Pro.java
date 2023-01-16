3da5e1bc89f36fe1722126aed48bc28384d17c44
Leonardo Caballero <leonardocaballero25@gmail.com>
2023-01-15 18:20:31 -0600

    first commit

---
+++ src/BADVIBES/Pro.java
@@ -0,0 +1,11 @@
+package BADVIBES;
+
+public class Pro {
+
+    public static void main(String[] args) {
+        Login ventana = new Login();
+        ventana.setVisible(true);//se muestra el formulario
+        ventana.setLocationRelativeTo(null); //se centra el formulario
+    }
+
+}
---
+++ src/BADVIBES/Proyecto.java
@@ -0,0 +1,14 @@
+
+package BADVIBES;
+
+public class Proyecto
+{
+
+    public static void main(String[] args)
+    {
+        Login ventana = new Login();
+        ventana.setVisible(true);//se muestra el formulario
+        ventana.setLocationRelativeTo(null); //se centra el formulario
+    }
+    
+}
